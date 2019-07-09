34a18d24f8507d793fd4948a60625b0afb10f358
gaudfra <francescogaudeni@outlook.it>
2019-07-09 11:13:51 +0200

    update classi

--- Progetto_GaudeniMaterazzi/src/main/java/progetto_po/Misurazioni.java
+++ Progetto_GaudeniMaterazzi/src/main/java/progetto_po/Misurazioni.java
@@ -1,5 +1,11 @@
 package progetto_po;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.String;
+
 public class Misurazioni {
     private double DMPS;
     private double CPC;
@@ -9,34 +15,88 @@ public class Misurazioni {
         this.DMPS = Double.parseDouble(dmps);
         this.CPC = Double.parseDouble(cpc);
     }
-}
 
-class date extends Misurazioni {
-    private int day;
-    private int month;
-    private int year;
+    @JsonIgnore
+    public int getDMPS(){
+        return DMPS;
+    }
 
-    date(String date, String dmps, String cpc) {
+    @JsonIgnore
+    public int getCPC(){
+        return CPC;
+    }
 
-        super(dmps, cpc);
-        String[] dmy = date.split("-");
-        this.day = Integer.parseInt(dmy[0]);
-        this.month = Integer.parseInt(dmy[1]);
-        this.year = Integer.parseInt(dmy[2]);
+    @JsonIgnore
+    public boolean equalmis(Misurazioni c) {
+        if (this.DMPS == c.getDMPS() && this.CPC == c.getCPC()) return true;
+        else return false;
+
     }
 }
 
-class time extends date {
+class datetime extends Misurazioni {
+    private int day;
+    private int month;
+    private int year;
     private int hour;
     private int minute;
     private double second;
 
-    time(String times, String date, String dmps, String cpc) {
+    datetime(String times, String date, String dmps, String cpc) {
 
-        super(date, dmps, cpc);
-        String[] hms = times.split(":");
+        super(dmps, cpc);
+        String[] dmy = datetime.split("-");
+        this.day = Integer.parseInt(dmy[0]);
+        this.month = Integer.parseInt(dmy[1]);
+        this.year = Integer.parseInt(dmy[2]);
+        String[] hms = datetime.split(":");
         this.hour = Integer.parseInt(hms[0]);
         this.minute = Integer.parseInt(hms[1]);
         this.second = Double.parseDouble(hms[2]);
     }
+
+
+    @JsonIgnore
+    public int getDay(){
+        return day;
+    }
+    @JsonIgnore
+    public int getMonth(){
+        return month;
+    }
+
+    @JsonIgnore
+    public int getYear(){
+        return year;
+    }
+
+
+    @JsonIgnore
+    public int getHour(){
+        return hour;
+    }
+    @JsonIgnore
+    public int getMinute(){
+        return minute;
+    }
+    @JsonIgnore
+    public double getSecond(){
+        return second;
+    }
+
+    @JsonIgnore
+    public boolean equaldatetime(datetime c) {
+        if (this.day == c.getDay() && this.month == c.getMonth() && this.year == c.getYear() && this.hour == c.getHour() && this.minute == c.getMinute() && this.second == getSecond()) return true;
+        else return false;
+
+    }
 }
+
+
+
+
+
+
+}
+
+
--- Progetto_GaudeniMaterazzi/src/main/java/progetto_po/scanner.java
+++ Progetto_GaudeniMaterazzi/src/main/java/progetto_po/scanner.java
@@ -3,6 +3,7 @@ package progetto_po;
         import java.io.File;
         import java.io.FileNotFoundException;
         import java.util.Scanner;
+        import java.util.ArrayList;
 
 public class scanner {
     public static void scannerizza(String FileName){
@@ -10,13 +11,13 @@ public class scanner {
         try {
             Scanner inputStream = new Scanner(file);
             inputStream.next(); // ignore the first line
-            time[] misurazione = new time[44990];
+            datetime[] misurazione = new datetime[][44990];
             int i = 0;
             while(inputStream.hasNext()){
                 String data = inputStream.next(); // gets a whole line
                 String[] SeparaData = data.split(" ");
                 String[] Separatempo = SeparaData[1].split(",");
-                misurazione[i] = new time(Separatempo[0], SeparaData[0], Separatempo[1], Separatempo[2]);
+                misurazione[i] = new datetime(Separatempo[0], SeparaData[0], Separatempo[1], Separatempo[2]);
                 i++;
             }
             inputStream.close();
