2a8b90d3f0d5aa531bb2a3afae8d60773199374e
gaudfra <francescogaudeni@outlook.it>
2019-07-10 14:41:09 +0200

    aggiornamento

--- Progetto_GaudeniMaterazzi/src/main/java/progetto_po/Misurazioni.java
+++ Progetto_GaudeniMaterazzi/src/main/java/progetto_po/Misurazioni.java
@@ -1,6 +1,10 @@
 package progetto_po;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.String;
 
 import java.io.Serializable;
 
@@ -14,21 +18,87 @@ public class Misurazioni implements Serializable {
         this.CPC = Double.parseDouble(cpc);
     }
 
-    @JsonIgnore
-    public double getDMPS(){
+
+    public int getDMPS(){
         return DMPS;
     }
 
-    @JsonIgnore
-    public double getCPC(){
+
+    public int getCPC(){
         return CPC;
     }
 
-    @JsonIgnore
+
     public boolean equalmis(Misurazioni c) {
         if (this.DMPS == c.getDMPS() && this.CPC == c.getCPC()) return true;
         else return false;
 
     }
 }
+
+class datetime extends Misurazioni {
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int minute;
+    private double second;
+
+    datetime(String times, String date, String dmps, String cpc) {
+
+        super(dmps, cpc);
+        String[] dmy = datetime.split("-");
+        this.day = Integer.parseInt(dmy[0]);
+        this.month = Integer.parseInt(dmy[1]);
+        this.year = Integer.parseInt(dmy[2]);
+        String[] hms = datetime.split(":");
+        this.hour = Integer.parseInt(hms[0]);
+        this.minute = Integer.parseInt(hms[1]);
+        this.second = Double.parseDouble(hms[2]);
+    }
+
+
+    @JsonIgnore
+    public int getDay(){
+        return day;
+    }
+    @JsonIgnore
+    public int getMonth(){
+        return month;
+    }
+
+    @JsonIgnore
+    public int getYear(){
+        return year;
+    }
+
+
+    @JsonIgnore
+    public int getHour(){
+        return hour;
+    }
+    @JsonIgnore
+    public int getMinute(){
+        return minute;
+    }
+    @JsonIgnore
+    public double getSecond(){
+        return second;
+    }
+
+    @JsonIgnore
+    public boolean equaldatetime(datetime c) {
+        if (this.day == c.getDay() && this.month == c.getMonth() && this.year == c.getYear() && this.hour == c.getHour() && this.minute == c.getMinute() && this.second == getSecond()) return true;
+        else return false;
+
+    }
+}
+
+
+
+
+
+
+}
+
 
